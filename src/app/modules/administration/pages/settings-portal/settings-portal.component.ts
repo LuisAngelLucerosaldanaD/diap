f622f4acc67d917beb04991fa4c8cf6f28f82d72
Luis Lucero <luis.lucero@bjungle.net>
2025-02-28 16:24:00 -0500

    feat images settings

--- src/app/modules/administration/pages/settings-portal/settings-portal.component.ts
+++ src/app/modules/administration/pages/settings-portal/settings-portal.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {FileHelper} from "../../../../core/utils/file/file";
+import {MessageService} from "primeng/api";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-settings-portal',
   standalone: true,
   imports: [],
   templateUrl: './settings-portal.component.html',
-  styleUrl: './settings-portal.component.scss'
+  styleUrl: './settings-portal.component.scss',
+  providers: [MessageService]
 })
 export class SettingsPortalComponent {
 
+  private _subscriptions: Subscription = new Subscription();
+  private readonly _toastService = inject(MessageService);
+
+  protected logoHome = signal('');
+  protected logoLogin = signal('');
+
+  protected processLogo(ev: any, type: string): void {
+    const file: File = ev.target.files[0];
+    if (!file) return;
+    if (file.type.split('/')[0] !== 'image') {
+      this._toastService.add({
+        severity: 'error',
+        summary: 'Módulo de Registro',
+        detail: 'Solo se permiten imágenes'
+      });
+      return;
+    }
+    this._subscriptions.add(
+      FileHelper.fileReader(file).subscribe({
+        next: res => {
+          if (type === 'home') this.logoHome.set(res);
+          else this.logoLogin.set(res);
+        },
+        error: (err) => {
+          console.error(err);
+          this._toastService.add({
+            severity: 'error',
+            summary: 'Módulo de Registro',
+            detail: 'No se pudo procesar la imagen'
+          });
+        }
+      })
+    );
+  }
 }
